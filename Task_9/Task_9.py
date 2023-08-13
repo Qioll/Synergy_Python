2089983b75f7d64dc4814fb9767c5e469401819f
Qioll <ksakharov.nick@gmail.com>
2023-08-13 20:34:38 +0300

    Task_9

---
+++ Task_9/Task_9.py
@@ -0,0 +1,34 @@
+# -*- coding: cp1251 -*-
+
+print('Задание №1')
+
+N = int(input("Введите количество чисел: "))
+
+numbers = list(map(int, input("Введите числа через пробел: ").split()))
+
+distinct_numbers = len(set(numbers))
+
+print("Количество различных чисел:", distinct_numbers)
+
+print('Задание №2')
+
+list1 = list(map(int, input("Введите числа первого списка через пробел: ").split()))
+
+list2 = list(map(int, input("Введите числа второго списка через пробел: ").split()))
+
+common_numbers = len(set(list1) & set(list2))
+
+print("Количество чисел, содержащихся одновременно в обоих списках:", common_numbers)
+
+print('Задание №3')
+
+numbers = input("Введите последовательность чисел через пробел: ").split()
+
+unique_numbers = set()
+
+for num in numbers:
+    if num not in unique_numbers:
+        unique_numbers.add(num)
+        print("NO")
+    else:
+        print("YES")
