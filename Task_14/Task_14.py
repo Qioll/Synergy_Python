c2a320e109c0a6a3d161323a39a0a20c90ac2ed6
Qioll <ksakharov.nick@gmail.com>
2023-08-13 21:13:43 +0300

    Task_14

--- Task_14/Task_14.py
+++ Task_14/Task_14.py
@@ -1,11 +1,11 @@
-# -*- coding: cp1251 -*-
+# -*- coding: UTF_8 -*-
 
 def print_sequence(my_list, index):
     if index < len(my_list):
         print(my_list[index])
         print_sequence(my_list, index + 1)
     else:
-        print("Конец списка")
+        print("РљРѕРЅРµС† СЃРїРёСЃРєР°")
 
 my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 print_sequence(my_list, 0)
