6b2a668d259a406c3a9a7ec0202d39456fc19207
Qioll <ksakharov.nick@gmail.com>
2023-08-13 20:35:19 +0300

    Task_13

---
+++ Task_13/Task_13.py
@@ -0,0 +1,40 @@
+# -*- coding: cp1251 -*-
+
+import random
+
+matrix1 = []
+for i in range(10):
+    row = []
+    for j in range(10):
+        row.append(random.randint(-50, 50))
+    matrix1.append(row)
+
+matrix2 = []
+for i in range(10):
+    row = []
+    for j in range(10):
+        row.append(random.randint(-50, 50))
+    matrix2.append(row)
+
+matrix3 = []
+for i in range(10):
+    row = []
+    for j in range(10):
+        row.append(matrix1[i][j] + matrix2[i][j])
+    matrix3.append(row)
+
+print("Первая матрица:")
+for row in matrix1:
+    print(row)
+
+print("\n")
+
+print("Вторая матрица:")
+for row in matrix2:
+    print(row)
+
+print("\n")
+
+print("Третья матрица (сумма первой и второй матриц):")
+for row in matrix3:
+    print(row)
